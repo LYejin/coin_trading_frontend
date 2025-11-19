a8468b4b15dc681e9de4f65f548f5872d5aa9d7c
LeeJunSung <jun130506@gmail.com>
2025-11-19 22:12:37 +0900

    퍼블리싱 임시 커밋

--- src/routers/Routers.js
+++ src/routers/Routers.js
@@ -2,11 +2,12 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { URL } from "../constants/router";
 import LoginForm from "../pages/login/loginForm";
+import Coin from "../pages/coin/Coin";
 
 const Routers = () => {
   return (
     <Routes>
-      <Route path={URL.home} element={<LoginForm />} />
+      <Route path={URL.home} element={<Coin />} />
     </Routes>
   );
 };
