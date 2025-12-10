29b0560a1e10ac4bf1e130b652acef15a8298826
LeeJunSung <jun130506@gmail.com>
2025-12-10 20:57:50 +0900

    커밋

--- src/pages/order/OrdersCard.js
+++ src/pages/order/OrdersCard.js
@@ -1,4 +1,50 @@
+import { useEffect } from "react";
+import Util from "../../Util/Util";
+
 const OrdersCard = () => {
+
+
+    /*
+        보유 수량 : balance
+        총 금액(현재 평가 금액) : balance * 현재가
+        투자 원금(평단 기준 평가) : balance * avg_buy_price
+        수익금 : 총 금액 - 투자 원금
+        수익률 : (수익금 / 투자 원금) * 100
+    */
+
+    useEffect(()=>{
+        getTotalAccounts();   
+    });
+
+    /**
+     * 
+     *  avg_buy_price: "0"
+        avg_buy_price_modified: true
+        balance: "10000.40059201"
+        currency: "KRW"
+        locked: "0"
+        unit_currency: "KRW"
+     */
+
+    const getTotalAccounts = async () => {
+        const accounts =  await Util.callApi("get", "/api/assets/getTotalAccounts");
+
+        console.log('accounts',accounts)
+
+
+    };
+
+    /**
+     * trade_price : 현재가
+     */
+    const getTickers = async () => {
+        const tickers = await Util.callApi("get", "/api/assets/tickers");
+
+        console.log('tickers',tickers);
+    };
+
+    
+
 
     return(
         <section className="card trade-card">
